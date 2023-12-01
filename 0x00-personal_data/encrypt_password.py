0c0836dd3161e22f80bf29aa16e1b5ab6e2f76e7
Abdulbasit <aabdulrafiu721@gmail.com>
2023-12-01 09:45:24 +0100

    validate password

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -10,3 +10,11 @@ def hash_password(password: str) -> bytes:
     hashed = bcrypt.hashpw(encoded, bcrypt.gensalt(14))
 
     return hashed
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    '''Checks if password and hashed password matches'''
+    encoded = password.encode()
+
+    result = bcrypt.checkpw(encoded, hashed_password)
+    return result
