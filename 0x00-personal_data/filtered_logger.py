4dcd23dd70c0f378cf38e5db67f9a40d6e676df6
Abdulbasit <aabdulrafiu721@gmail.com>
2023-12-01 07:50:21 +0100

    Implement type annotation

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 '''Masks sensitive data'''
 import re
+from typing import List
 
 
-def filter_datum(fields, redaction, message, separator):
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
     '''Use regex to mask specified fields'''
     for i in fields:
         message = re.sub(f'{i}=.*?{separator}',
